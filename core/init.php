ee76e2f464842b075247fa66aa6d61a9d070fc5d
Yuriy Plotnikov <yuriy.plotnikov@travelline.ru>
2025-05-01 22:37:08 +0300

    Добавление английского языка, исправление ошибок, мелкие правки

--- core/init.php
+++ core/init.php
@@ -1,3 +1,10 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/core/languages.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/core/classes/Tools.php';
+
+spl_autoload_register(function ($class) {
+    $file = $_SERVER['DOCUMENT_ROOT'] . '/core/classes/' . $class . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
