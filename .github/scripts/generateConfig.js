9163ce163101df9ba6807a83a46b329068dc4e75
Yuriy Plotnikov <yuriy.plotnikov@travelline.ru>
2025-05-09 10:32:06 +0300

    Настройка GitHub Actions

--- .github/scripts/generateConfig.js
+++ .github/scripts/generateConfig.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
+const filePath = path.join(process.env.GITHUB_WORKSPACE, 'config.json');
 const apiUrl = process.env.API_URL;
 
 if (!apiUrl) {
@@ -12,7 +13,6 @@ const content = JSON.stringify({
   apiBaseUrl: apiUrl
 }, null, 4);
 
-const filePath = path.join(process.env.GITHUB_WORKSPACE, 'config.json');
 
 fs.writeFileSync(filePath, content, 'utf8');
 console.log('Config is have been generated.');
