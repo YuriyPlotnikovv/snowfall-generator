2c2341b1d385e2888aaaf3ae3e65b8fab1551be2
Yuriy Plotnikov <yuriy.plotnikov@travelline.ru>
2025-10-10 14:02:30 +0300

    Add donate badge

--- include/header.php
+++ include/header.php
@@ -45,7 +45,7 @@ $badgeLabel = urlencode($MESS['SUPPORT_THE_AUTHOR']);
                 </a>
             </div>
 
-            <a href="https://yoomoney.ru/fundraise/1DA99C0NRL2.251010">
+            <a class="header__badge" href="https://yoomoney.ru/fundraise/1DA99C0NRL2.251010" target="_blank">
                 <img src="https://img.shields.io/badge/YooMoney-32CD32?style=for-the-badge&label=<?= $badgeLabel ?>" alt="<?= $MESS['SUPPORT_THE_AUTHOR'] ?>"/>
             </a>
 
